c3ae122d4e491936cb4340aa5d717a088c12ce6d
nadeneAmara <nadeneabuamara@gmail.com>
2020-07-01 14:20:36 -0700

    formatted corpus into array of overlapping sequences

--- data_prep.py
+++ data_prep.py
@@ -11,4 +11,24 @@ raw = response.read().decode('utf8')
 start = raw.find("CHAPTER I")
 end = raw.rfind("***END OF THE PROJECT GUTENBERG")
 raw = raw[start:end]
-print(type(raw))
+
+# Get set of unique characters as our features
+raw = raw.lower()
+raw_len = len(raw)
+features = sorted(list(set(raw)))
+num_features = len(features)
+feature_index = dict((f, i) for i, f in enumerate(features))
+
+# Length of character sequences, chunked out in steps of 3
+maxLen = 40
+step = 3
+sequences = []
+next_char = []
+for i in range(0, raw_len - maxLen, step):
+    sequences.append(raw[i:i + maxLen])
+    next_char.append(raw[i + maxLen])
+
+
+
+
+
