e8e8bd93f096d11f024e3bf36051864d39920454
nadeneAmara <nadeneabuamara@gmail.com>
2020-10-15 02:29:35 -0700

    changed sample text. removed punctuation. ready to start on model.

--- data_prep.py
+++ data_prep.py
@@ -4,51 +4,52 @@ from nltk import tokenize
 from nltk import word_tokenize
 from urllib import request
 import numpy as np
+import string
 
 # Load in Jane Eyre from Project Gutenberg 
-url = "http://www.gutenberg.org/cache/epub/1260/pg1260.txt"
+url = "http://www.gutenberg.org/files/58585/58585-0.txt"
 response = request.urlopen(url)
 raw = response.read().decode('utf8')
 # Remove unwanted text
-start = raw.find("CHAPTER I")
-end = raw.rfind("***END OF THE PROJECT GUTENBERG")
+start = raw.find("Almustafa")
+end = raw.rfind("*** END OF THIS PROJECT")
 raw = raw[start:end]
 
 # Get set of unique characters as our features
 raw = raw.lower()
 raw_len = len(raw)
-features = sorted(list(set(word_tokenize(raw))))
-print(features)
+tokens = word_tokenize(raw)
+words = [word for word in tokens if word.isalpha()]
+#features = sorted(list(set(word_tokenize(raw))))
+features = sorted(list(set(words)))
 num_features = len(features)
+print(num_features)
 feature_index = dict((f, i) for i, f in enumerate(features))
 raw = tokenize.sent_tokenize(raw)
 
-# Length of character sequences, chunked out in steps of 3
-#maxLen = 40
-step = 3
-sequences = []
-next_char = []
-
 # Generate overlapping sequences of words
-# TODO: Find longest sequence length, pad the rest
+sequences = []
+next_word = []
 maxLen = 0
 for sequence in raw:
     token_list = word_tokenize(sequence)
+    token_list = [token for token in token_list if token.isalpha()]
     i = 0
     if (len(token_list) > maxLen):
         maxLen = len(token_list)
-    while (i < len(token_list)):
+    while (i < (len(token_list)-1)):
         n_gram = token_list[:i+1]
         sequences.append(n_gram)
+        next_word.append(token_list[i+1])
         i = i + 1
-
+print(sequences)
 x = np.zeros((len(sequences), maxLen, num_features), dtype=np.bool)
 y = np.zeros((len(sequences), num_features), dtype=np.bool)
 
 for i, sequence in enumerate(sequences):
     for j, char in enumerate(sequence):
         x[i, j, feature_index[char]] = 1
-    y[i, feature_index[next_char[i]]] = 1
+    y[i, feature_index[next_word[i]]] = 1
 
 
 
