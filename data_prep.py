08181ee636c2659f6c9ccfd79d4c98c4ffd19196
nadeneAmara <nadeneabuamara@gmail.com>
2020-10-20 20:52:26 -0700

    not doing one hot encoding anymore, prepared text for word embedding layer

--- data_prep.py
+++ data_prep.py
@@ -4,9 +4,15 @@ import nltk, re, pprint
 from nltk import tokenize
 from nltk import word_tokenize
 from urllib import request
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
+from keras.preprocessing.sequence import pad_sequences
 
 
-# Load in Jane Eyre from Project Gutenberg 
+# Load in The Prophet from Project Gutenberg 
 url = "http://www.gutenberg.org/files/58585/58585-0.txt"
 response = request.urlopen(url)
 raw = response.read().decode('utf8')
@@ -15,43 +21,34 @@ start = raw.find("Almustafa")
 end = raw.rfind("*** END OF THIS PROJECT")
 raw = raw[start:end]
 
-# Get set of unique characters as our features
+# Make text all lowercase and split into sentences
 raw = raw.lower()
 raw_len = len(raw)
-tokens = word_tokenize(raw)
-words = [word for word in tokens if word.isalpha()]
-#features = sorted(list(set(word_tokenize(raw))))
-features = sorted(list(set(words)))
-num_features = len(features)
-print(num_features)
-feature_index = dict((f, i) for i, f in enumerate(features))
 raw = tokenize.sent_tokenize(raw)
 
 # Generate overlapping sequences of words
 sequences = []
-next_word = []
 maxLen = 0
 for sequence in raw:
     token_list = word_tokenize(sequence)
     token_list = [token for token in token_list if token.isalpha()]
     i = 0
-    if (len(token_list) > maxLen):
-        maxLen = len(token_list)
     while (i < (len(token_list)-1)):
-        n_gram = token_list[:i+1]
-        sequences.append(n_gram)
-        next_word.append(token_list[i+1])
+        tokens = token_list[:i+1]
+        line = ' '.join(tokens)
+        sequences.append(line)
         i = i + 1
-print(sequences)
-x = np.zeros((len(sequences), maxLen, num_features), dtype=np.bool)
-y = np.zeros((len(sequences), num_features), dtype=np.bool)
 
-for i, sequence in enumerate(sequences):
-    for j, char in enumerate(sequence):
-        x[i, j, feature_index[char]] = 1
-    y[i, feature_index[next_word[i]]] = 1
-
-
-
+# Map our words to integer values and split sequences into 
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(sequences)
+sequences = tokenizer.texts_to_sequences(sequences)
+maxLen = max([len(x) for x in sequences])
+num_vocab = len(tokenizer.word_index) + 1
+input_sequences = np.array(pad_sequences(sequences, maxlen = maxLen-1, padding = 'pre'))
+x = input_sequences[:,:-1]
+y = input_sequences[:,-1]
+y = to_categorical(y, num_classes=num_vocab)
+len_sequence = x.shape[1]
 
 
