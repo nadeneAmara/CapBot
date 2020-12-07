d50171358de30aad1e9acb0ce0e8518277b90429
nadeneAmara <nadeneabuamara@gmail.com>
2020-12-06 19:40:52 -0800

    refactored code, added embedding to model

--- data_prep.py
+++ data_prep.py
@@ -11,44 +11,51 @@ from keras.preprocessing.text import Tokenizer
 from keras.utils import to_categorical
 from keras.preprocessing.sequence import pad_sequences
 
-
-# Load in The Prophet from Project Gutenberg 
-url = "http://www.gutenberg.org/files/58585/58585-0.txt"
-response = request.urlopen(url)
-raw = response.read().decode('utf8')
-# Remove unwanted text
-start = raw.find("Almustafa")
-end = raw.rfind("*** END OF THIS PROJECT")
-raw = raw[start:end]
-
-# Make text all lowercase and split into sentences
-raw = raw.lower()
-raw_len = len(raw)
-raw = tokenize.sent_tokenize(raw)
+def load_text(url):
+    response = request.urlopen(url)
+    raw = response.read().decode('utf8')
+    # Remove unwanted text
+    start = raw.find("Almustafa")
+    end = raw.rfind("*** END OF THIS PROJECT")
+    raw = raw[start:end]
+    # Make text all lowercase and split into sentences
+    raw = raw.lower()
+    raw_len = len(raw)
+    raw = tokenize.sent_tokenize(raw)
+    return raw
 
 # Generate overlapping sequences of words
-sequences = []
-maxLen = 0
-for sequence in raw:
-    token_list = word_tokenize(sequence)
-    token_list = [token for token in token_list if token.isalpha()]
-    i = 0
-    while (i < (len(token_list)-1)):
-        tokens = token_list[:i+1]
-        line = ' '.join(tokens)
-        sequences.append(line)
-        i = i + 1
+def get_sequences(raw):
+    sequences = []
+    maxLen = 0
+    for sequence in raw:
+        token_list = word_tokenize(sequence)
+        token_list = [token for token in token_list if token.isalpha()]
+        i = 0
+        while (i < (len(token_list)-1)):
+            tokens = token_list[:i+1]
+            line = ' '.join(tokens)
+            sequences.append(line)
+            i = i + 1
+    return sequences
 
 # Map our words to integer values and split sequences into 
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(sequences)
-sequences = tokenizer.texts_to_sequences(sequences)
-maxLen = max([len(x) for x in sequences])
-num_vocab = len(tokenizer.word_index) + 1
-input_sequences = np.array(pad_sequences(sequences, maxlen = maxLen-1, padding = 'pre'))
-x = input_sequences[:,:-1]
-y = input_sequences[:,-1]
-y = to_categorical(y, num_classes=num_vocab)
-len_sequence = x.shape[1]
+def prepare_sequences(sequences):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(sequences)
+    sequences = tokenizer.texts_to_sequences(sequences)
+    maxLen = max([len(x) for x in sequences])
+    num_vocab = len(tokenizer.word_index) + 1
+    input_sequences = np.array(pad_sequences(sequences, maxlen = maxLen-1, padding = 'pre'))
+    x = input_sequences[:,:-1]
+    y = input_sequences[:,-1]
+    y = to_categorical(y, num_classes=num_vocab)
+    len_sequence = x.shape[1]
+    return x, y, maxLen, num_vocab
+
+
+
+
+
 
 
--- model.py
+++ model.py
@@ -1,8 +1,21 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from data_prep import *
 
-def create_model(x, y, maxLen, num_features):
-    model = sequential()
-    model.add(LSTM(128, input_shape=(maxLen, num_features)))
+def create_model(maxLen, num_features):
+    model = Sequential()
+    model.add(Embedding(num_features, 50, input_length=maxLen))
+    model.add(LSTM(100))
+    model.add(Dense(100, activation='relu'))
     model.add(Dense(num_features, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    return model
+
+# Load in The Prophet from Project Gutenberg 
+url = "http://www.gutenberg.org/files/58585/58585-0.txt"
+text = load_text(url)
+sequences = get_sequences(text)
+x, y, maxLen, num_features = prepare_sequences(sequences)
+model = create_model(maxLen, num_features)
+model.fit(x, y, epochs=100)
