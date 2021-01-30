4cba3bc015a43024890d8673c23008c185acebf7
nadeneAmara <nadeneabuamara@gmail.com>
2021-01-30 01:00:18 -0800

    updated files to match jupyter notebook

--- data_prep.py
+++ data_prep.py
@@ -4,25 +4,65 @@ import nltk, re, pprint
 from nltk import tokenize
 from nltk import word_tokenize
 from urllib import request
+import json
+import time
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from keras.preprocessing.text import Tokenizer
 from keras.utils import to_categorical
 from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Embedding, LSTM, Dense
+from pickle import dump
+import requests
 
-def load_text(url):
-    response = request.urlopen(url)
-    raw = response.read().decode('utf8')
-    # Remove unwanted text
-    start = raw.find("Almustafa")
-    end = raw.rfind("*** END OF THIS PROJECT")
-    raw = raw[start:end]
-    # Make text all lowercase and split into sentences
-    raw = raw.lower()
-    raw_len = len(raw)
-    raw = tokenize.sent_tokenize(raw)
-    return raw
+# Retrieve comments from pushshift reddit database
+def request_comments(**kwargs):
+  response = requests.get("https://api.pushshift.io/reddit/comment/search/",params=kwargs)
+  data = response.json()
+  comments = data['data']
+  return comments
+
+# Get text from comments
+def get_comment_set(comment_number):
+  sr_names = ["aww","wholesomememes","funny", "interestingasfuck", "EarthPorn"]
+  comment_bodies = ""
+  before = None
+  for i in sr_names:
+    comments_left = comment_number
+    while (comments_left > 0):
+      comments = request_comments(subreddit=i, size=100, before=before, sort='desc',sort_type='created_utc')
+      for comment in comments:
+        comment_bodies = comment_bodies + comment['body']
+        before = comment['created_utc']
+      comments_left = comments_left - 100
+      time.sleep(2)
+  return comment_bodies
+
+# Save comments to file, line by line
+def save_doc(sequences, filename):
+    dataset = '\n'.join(sequences)
+    file = open(filename, 'w')
+    file.write(dataset)
+    file.close()
+
+# Load comments from file
+def load_doc(filename):
+    file = open(filename, 'r')
+    text = file.read()
+    file.close()
+    return text
+
+def load_text():
+  # Make text all lowercase and split into sentences
+  # Load in new raw here
+  raw = get_comment_set(200)
+  raw = raw.lower()
+  raw_len = len(raw)
+  raw = tokenize.sent_tokenize(raw)
+  print(len(raw))
+  return raw
 
 # Generate overlapping sequences of words
 def get_sequences(raw):
@@ -37,6 +77,8 @@ def get_sequences(raw):
             line = ' '.join(tokens)
             sequences.append(line)
             i = i + 1
+    filename = 'reddit_comments.txt'
+    save_doc(sequences, filename)
     return sequences
 
 # Map our words to integer values and split sequences into 
@@ -51,11 +93,9 @@ def prepare_sequences(sequences):
     y = input_sequences[:,-1]
     y = to_categorical(y, num_classes=num_vocab)
     len_sequence = x.shape[1]
-    return x, y, maxLen, num_vocab
-
-
-
-
+    # save the tokenizer
+    dump(tokenizer, open('tokenizer.pkl', 'wb'))
+    return x, y, len_sequence, num_vocab
 
 
 
--- model.py
+++ model.py
@@ -1,6 +1,10 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+import pickle
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import random
 from data_prep import *
 
 def create_model(maxLen, num_features):
@@ -12,10 +16,50 @@ def create_model(maxLen, num_features):
     model.compile(loss='categorical_crossentropy', optimizer='adam')
     return model
 
-# Load in The Prophet from Project Gutenberg 
-url = "http://www.gutenberg.org/files/58585/58585-0.txt"
-text = load_text(url)
-sequences = get_sequences(text)
+#text = load_text()
+#s = get_sequences(text)
+filename = 'reddit_comments.txt'
+sequences = load_doc(filename)
+sequences = sequences.split('\n')
 x, y, maxLen, num_features = prepare_sequences(sequences)
 model = create_model(maxLen, num_features)
-model.fit(x, y, epochs=100)
+
+path = F"/content/drive/MyDrive/captbot.ckpt" 
+#Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=path,
+                                                 save_weights_only=False,
+                                                 verbose=1)
+model.fit(x, y, epochs=150, callbacks=[cp_callback])
+
+def generate_seq_diverse(model, tokenizer, seq_length, seed_text, n_words):
+  result = list()
+  in_text = seed_text
+    # generate a fixed number of words
+  for _ in range(n_words):
+        # encode the text as integer
+    encoded = tokenizer.texts_to_sequences([in_text])[0]
+        # truncate sequences to a fixed length
+    encoded = pad_sequences([encoded], maxlen=seq_length, truncating='pre')
+        # predict probabilities for each word
+    probabilities = model.predict(encoded)
+    predictions = []
+    for word, index in tokenizer.word_index.items():
+      predictions.append({'text': in_text + ' ' + word, 
+                          'score': probabilities[0][index]})
+    predictions = sorted(predictions, key=lambda p: p['score'], reverse=True)
+    top_predictions = []
+    top_score = predictions[0]['score']
+    min_score = 0.6
+    rand_value = random.randint(int(min_score * 1000),1000)
+    for p in predictions:
+      if p['score'] >= rand_value/1000*top_score:
+        top_predictions.append(p)
+    random.shuffle(top_predictions)
+    in_text = top_predictions[0]['text']
+  return in_text
+
+# load model 
+new_model = tf.keras.models.load_model("/content/drive/My Drive/captbot.ckpt")
+# load tokenizer
+tokenizer = pickle.load(open('/content/drive/My Drive/tokenizer.pkl', 'rb'))
+
