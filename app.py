87c0c4649ea5cef70adbfcf710041b9ad20c1831
nadeneAmara <nadeneabuamara@gmail.com>
2021-02-10 01:48:30 -0800

    added flask app functions, form to html

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from model.py import *
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    pass
+    return render_template('index.html')
+    #caption = main()
+
+@app.route('/generate',methods=['POST'])
+def generate():
+    url = request.form['url']
+    caption = main(url)
+    return render_template('index.html', caption_text = '$ {}'.format(caption))
