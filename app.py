e2a966abf69671a59999bf68b9d94e2b6fa19587
nadeneAmara <nadeneabuamara@gmail.com>
2021-02-11 01:34:06 -0800

    Added form to html, fixed flask request error

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, jsonify, request
-from model.py import *
+from flask import Flask, render_template, jsonify
+from flask import request as flask_request
+from model import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-    #caption = main()
 
 @app.route('/generate',methods=['POST'])
 def generate():
-    url = request.form['url']
+    url = flask_request.form['url']
     caption = main(url)
     return render_template('index.html', caption_text = '$ {}'.format(caption))
+
+if __name__ == "__main__":
+    app.run(debug=True)
